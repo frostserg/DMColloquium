989cd11e50fd574582566882d419e28e9094ed7b
frostserg <frostsegei01@gmail.com>
2015-05-12 19:51:05 +0300

    Create DIV_NN_Dk.h

---
+++ DIV_NN_Dk.h
@@ -0,0 +1,30 @@
+//
+//  NaturalNumber.h
+//  DMColloquium
+//
+//  Created by Ilya Kulebyakin on 4/28/15.
+//  Copyright (c) 2015 Ilya Kulebyakin. All rights reserved.
+//
+
+#ifndef __DMColloquium__NaturalNumber__
+#define __DMColloquium__NaturalNumber__
+
+#include <vector>
+
+
+
+class NaturalNumber {
+protected:
+    // you must only use first 9 digits of each int.
+    // So, if a number is: 12345678901234 than
+    // digitBlocks[1] = 12345
+    // digitBlocks[0] = 678901234
+    std::vector<unsigned int> digitBlocks;
+public:
+    NaturalNumber();
+    NaturalNumber(std::vector<unsigned int> digits):digitBlocks{digits}{};
+    NaturalNumber DIV_NN_Dk(NaturalNumber x1, NaturalNumber x2);
+    // put declarations-ONLY for each method below
+};
+
+#endif /* defined(__DMColloquium__NaturalNumber__) */
